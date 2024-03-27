a74010152f6d380cc1c1610de8c57a570cf353e7
JRParreno <jhonrhayparreno22@gmail.com>
2024-03-27 10:43:07 +0800

    remove unused comments

--- academic_record/student_views.py
+++ academic_record/student_views.py
@@ -84,58 +84,11 @@ class StudentAssessmentListView(generics.ListAPIView):
                 academic_year=current_academic, student__user__pk=user.pk)
 
             if register_users.exists():
-                # check wether complete subjects
-                # set is_view_grade = true to display grades
                 # check the user wether is register to current academic or not
                 register_user = register_users.first()
                 return StudentAssessment.objects.filter(assessment__academic_year=current_academic, assessment__grading_period=grading_period, student=register_user.student).order_by('created_at', 'assessment__grading_period')
 
         return []
-
-    def list(self, request, *args, **kwargs):
-        response = super().list(request, *args, **kwargs)
-
-        # Get all student assessment results
-        student_assessments = self.get_queryset()
-
-        written_works_marks = []
-        performance_tasks_marks = []
-        quarterly_assessments_marks = []
-        written_weightage = 0
-        performance_task_weightage = 0
-        quarterly_assessment_weightage = 0
-
-        # Calculate the total weighted marks
-        for sa in student_assessments:
-            if sa.assessment.assessment_type == 'WRITTEN_WORKS':
-                written_works_marks.append(
-                    sa.obtained_marks/sa.assessment.max_marks)
-                if written_weightage == 0:
-                    written_weightage = sa.assessment.subject.written_work / 100
-            if sa.assessment.assessment_type == 'PERFORMANCE_TASK':
-                performance_tasks_marks.append(
-                    sa.obtained_marks/sa.assessment.max_marks)
-                if performance_task_weightage == 0:
-                    performance_task_weightage = sa.assessment.subject.performance_task / 100
-            if sa.assessment.assessment_type == 'QUARTERLY_ASSESSMENT':
-                quarterly_assessments_marks.append(
-                    sa.obtained_marks/sa.assessment.max_marks)
-                if quarterly_assessment_weightage == 0:
-                    quarterly_assessment_weightage = sa.assessment.subject.quartery_assessment / 100
-
-        written_works = {"weightage": written_weightage,
-                         "obtained_marks": written_works_marks, }
-        performance_tasks = {"weightage": performance_task_weightage,
-                             "obtained_marks": performance_tasks_marks, }
-        quarterly_assessments = {
-            "weightage": quarterly_assessment_weightage, "obtained_marks": quarterly_assessments_marks, }
-
-        my_gap = gpa_calculate(
-            written_works, performance_tasks, quarterly_assessments)
-
-        # Modify the response data to include the average GPA
-        response.data['gpa'] = my_gap
-        return response
 
 
 class StudentOverAllGPAView(APIView):
