f380313207721a1cc5edc5128396c6c0b5422f4b
JRParreno <jhonrhayparreno22@gmail.com>
2024-04-05 10:53:56 +0800

    fix asgi for https

--- ease_studyante_core/asgi.py
+++ ease_studyante_core/asgi.py
@@ -9,25 +9,39 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat import routing
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
 
 django_asgi_app = get_asgi_application()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ease_studyante_core.settings')
 
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(
-                routing.websocket_urlpatterns
-            )
+# Development mode: HTTP
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             routing.websocket_urlpatterns
+#         )
+#     )
+# })
+# Production mode: HTTPS
+
+application = ProtocolTypeRouter({
+    "http": AuthMiddlewareStack(
+        AllowedHostsOriginValidator(
+            get_asgi_application()
         )
-    }
-)
+    ),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    )
+})
 
 ASGI_APPLICATION = 'ease_studyante_core.asgi.application'
