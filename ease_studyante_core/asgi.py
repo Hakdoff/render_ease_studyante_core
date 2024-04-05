6127c550a57ddb610d96c356bb5eba23c552752d
JRParreno <jhonrhayparreno22@gmail.com>
2024-04-05 13:21:15 +0800

    added django setup

--- ease_studyante_core/asgi.py
+++ ease_studyante_core/asgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
 
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
@@ -30,6 +31,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ease_studyante_core.settings')
 #     )
 # })
 # Production mode: HTTPS
+django.setup()
 
 application = ProtocolTypeRouter({
     "https": AuthMiddlewareStack(
