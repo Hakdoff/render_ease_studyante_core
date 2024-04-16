82e586511cf385debf9d507128b35b17d0002573
JRParreno <jhonrhayparreno22@gmail.com>
2024-04-16 19:39:48 +0800

    fix filter assessments

--- academic_record/custom_filter_assessment.py
+++ academic_record/custom_filter_assessment.py
@@ -5,17 +5,19 @@ from django.db.models import Q, F
 class CustomFilterAssessment(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         search_fields = request.query_params.get('search_fields')
+        search_values = request.query_params.get('search_values')
         name = request.query_params.get('name', None)
 
         if search_fields:
             search_fields = search_fields.split(',')
-            or_query = Q()
-            for field in search_fields:
-                field_value = request.query_params.get(field)
-                if field_value:
-                    or_query &= Q(**{f'{field}': field_value})
-            queryset = queryset.filter(
-                or_query).distinct()
+            search_values = search_values.split(',')
+            if len(search_fields) == len(search_values):
+                and_query = Q()
+                for field, value in zip(search_fields, search_values):
+                    if value and value != 'ALL':
+                        and_query &= Q(**{f'{field}': value})
+                queryset = queryset.filter(
+                    and_query).distinct()
 
         if name:
             queryset = queryset.filter(name__icontains=name)
