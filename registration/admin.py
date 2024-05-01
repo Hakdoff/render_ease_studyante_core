7288a9cf1bf91389efe04cd9f064f612569d3645
Erika Fuerte <erikafuerte09@gmail.com>
2024-05-01 12:11:35 +0800

    Added validation in registration

--- registration/admin.py
+++ registration/admin.py
@@ -1,6 +1,23 @@
+from django import forms
 from django.contrib import admin
 from .models import Registration
 
+class RegistrationAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Registration
+        fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super().clean()
+        student = cleaned_data.get('student')
+
+        if student:
+            # Check if the student is already registered
+            if Registration.objects.filter(student=student).exists():
+                self.add_error('student', 'This student is already registered.')
+        
+        return cleaned_data
 
 @admin.register(Registration)
 class RegistrationAdminView(admin.ModelAdmin):
@@ -8,6 +25,7 @@ class RegistrationAdminView(admin.ModelAdmin):
                      'student__user__first_name', 'section__name']
     list_display = ['student', 'contact_number', 'section', 'academic_year']
     list_filter = ['student', 'section',]
+    form = RegistrationAdminForm
     autocomplete_fields = ['student', 'section', 'academic_year']
 
     def contact_number(self, obj):
